dd2238f4e740facd032d98238b6eb7fdb5bdd1ce
Krushna Kharat <krushnakharat.official@gmail.com>
2025-07-17 11:18:37 +0530

    helluia

--- quiz-app-backend/routes/quizRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getAllQuizzes, createQuiz } = require('../controllers/quizController');
-const { authMiddleware } = require('../middleware/authMiddleware');
-
-router.get('/', authMiddleware, getAllQuizzes);
-router.post('/', authMiddleware, createQuiz);
-
-module.exports = router;
