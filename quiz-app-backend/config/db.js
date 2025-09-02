b50188a155a4f736e9d613b95de274c9f619530a
Krishaaa <120458485+KrushnaKharat@users.noreply.github.com>
2025-09-02 14:33:05 +0530

    Update db.js

--- quiz-app-backend/config/db.js
+++ quiz-app-backend/config/db.js
@@ -1,8 +1,7 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString:
-   
+  connectionString: 
   "postgresql://postgres.zwgeerzhxypxljlgseig:Appliedinsights%402202@aws-1-ap-south-1.pooler.supabase.com:6543/postgres",
   ssl: { rejectUnauthorized: false },
 });
