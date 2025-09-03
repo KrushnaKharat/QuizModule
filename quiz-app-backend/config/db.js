3d5ec636d0c727a23471c4526ca6922909c4b653
Krushna Kharat <krushnakharat.official@gmail.com>
2025-09-03 11:25:43 +0530

    heelluu

--- quiz-app-backend/config/db.js
+++ quiz-app-backend/config/db.js
@@ -2,7 +2,8 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   connectionString:
-    "postgresql://postgres.bajmywmsooalvdvoteng:Appliedinsights%402202@aws-1-ap-south-1.pooler.supabase.com:6543/postgres",
+    // "postgresql://postgres:Appliedinsights%402202@db.zwgeerzhxypxljlgseig.supabase.co:5432/postgres",
+    "postgresql://postgres.zwgeerzhxypxljlgseig:appliedinsights2202@aws-1-ap-south-1.pooler.supabase.com:6543/postgres",
   ssl: { rejectUnauthorized: false },
 });
 
