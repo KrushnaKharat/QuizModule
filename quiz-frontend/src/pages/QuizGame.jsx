f0081bc5bc1fe99a2e9b79ba7d2989e281b93d17
Krushna Kharat <krushnakharat.official@gmail.com>
2025-09-18 12:33:44 +0530

    sdfs

--- quiz-frontend/src/pages/QuizGame.jsx
+++ quiz-frontend/src/pages/QuizGame.jsx
@@ -1,13 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function QuizGame() {
   return (
     <div>
-      <button>
-        Host Quiz
-      </button>
+      <button>Host Quiz</button>
     </div>
-  )
+  );
 }
 
-export default QuizGame
+export default QuizGame;
