25c251d91f52c04dd9d9db8fdb90906ef8d7857d
Vaibhav gond <gondvaibhav11@gmail.com>
2025-09-26 13:58:50 +0530

    added loader

--- quiz-frontend/src/pages/Loader.jsx
+++ quiz-frontend/src/pages/Loader.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Loader({ text = "Loading..." }) {
   return (
-    <div className="flex flex-col items-center justify-center h-screen w-full">
+    <div className="flex flex-col items-center justify-center max-h-96 w-full">
       <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-b-4 border-indigo-500 mb-4"></div>
       <span className="text-indigo-700 font-semibold text-lg">{text}</span>
     </div>
